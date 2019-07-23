bb2723d7b9328779826fe2988e89182b409d396e
Henry Blevins <heblevi@gmail.com>
2019-07-23 15:14:04 -0400

    Complete recitation

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-const About = () => <h1>About</h1>;
+const About = (props) => {
+  console.log('About', props);
+  console.log(props.match.params.id);
+  return (
+    <h1>
+      About {props.match.params.id}
+      <Route path={`${props.match.path}/henry`} render={props => {
+        console.log('Henry', props);
+        return <div>Henry</div>;
+      }}/>
+      <Route path={`${props.match.path}/brad`}render={props => <div>Brad</div>}/>
+      <button onClick={() => {
+        console.log('We Clicked the Button!');
+        props.history.push('/');
+      }}>Navigate to Home</button>
+      <button onClick={() => {
+        console.log('We Clicked Back!');
+        props.history.goBack();
+      }}>Go Back</button>
+    </h1>
+  );
+};
 
 export default About;
